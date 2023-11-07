a1b6a281dbbc8c53b8b5ec5fa008d60b208a0776
Dave Bailey <dave.andrew.bailey@gmail.com>
2023-11-07 15:33:44 -0500

    style: left container

--- components/ResupplyCalculator.vue
+++ components/ResupplyCalculator.vue
@@ -6,21 +6,37 @@ const store = useResupplyCalculatorStore()
 
 
 <template>
-  <div class="total-values">
-    <div>Days Until Next Resupply: {{  store.daysUntilNextResupply }}</div> 
-    <div>Caloric Need for Section: {{ store.caloriesUntilNextResupply }}</div>
+  <div class="resupply-calculator">
+    <div class="resupply-calculator__left-container">
+      <div class="resupply-calculator__totals">
+        <div>Caloric Need for Section: {{ store.caloriesUntilNextResupply }}</div>
+        <div>Days Until Next Resupply: {{  store.daysUntilNextResupply }}</div> 
+      </div>
+    
+      <total-days-and-calories />
+    </div>
+    <add-resupply-item />
   </div>
-
-  <total-days-and-calories />
-  <add-resupply-item />
 </template>
 
 <style lang="scss" scoped>
-.total-values {
-  border: 2px solid green;
+.resupply-calculator {
   display: flex;
-  flex-direction: column;
-  padding: 2em;
+  justify-content: space-around;
+
+  &__left-container {
+    border: 2px solid black;
+    border-radius: .5em;
+    display: flex;
+    flex-direction: column;
+  }
+
+  &__totals {
+    display: flex;
+    flex-direction: column;
+    font-size: 1.5em;
+    padding: 2em;
+  }
 }
 
 </style>
