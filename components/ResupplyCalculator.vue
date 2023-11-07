d31f2fa0167abf1cabf56c6418c1b71cb39b7974
Dave Bailey <dave.andrew.bailey@gmail.com>
2023-11-07 12:34:29 -0500

    refactor: use TotalDaysAndCalories and store

--- components/ResupplyCalculator.vue
+++ components/ResupplyCalculator.vue
@@ -1,57 +1,12 @@
-<script setup lang="ts">
-const dailyCalories = useState<number>('dailyCalories', () => 0)
-const dailyMileage = useState<number>('dailyMileage', () => 0)
-const totalMileageUntilResupply = useState<number>('totalMileageUntileResupply', () => 0)
-const daysUntilNextResupply = useState<number>('daysUntilNextResupply', () => 0)
-const caloriesUntilNextResupply = useState<number>('caloriesUntilNextResupply', () => 0)
+<script setup>
+import { useResupplyCalculatorStore } from '../store/useResupplyCalculatorStore';
 
-const daysToCompleteSection = () => {
-  if (totalMileageUntilResupply.value === 0 || dailyMileage.value === 0) return
-
-  daysUntilNextResupply.value = totalMileageUntilResupply.value / dailyMileage.value
-
-  caloriesPerSection()
-}
-
-const caloriesPerSection = () => {
-  caloriesUntilNextResupply.value = dailyCalories.value * daysUntilNextResupply.value
-}
+const store = useResupplyCalculatorStore()
 </script>
 
+
 <template>
-  Days Until Next Resupply {{  daysUntilNextResupply }}<br>
-  Caloric Need for Section {{ caloriesUntilNextResupply }}
-  <div>
-    <label>Calories Per Day
-        <input 
-          type="number"
-          v-model.number="dailyCalories"
-          placeholder="Add Your Daily Calories"
-          v-on:input="daysToCompleteSection()"
-          required
-        />
-    </label>
-  </div>
-  <div>
-    <label>Miles Per Day
-      <input
-        type="number"
-        v-model.number="dailyMileage"
-        placeholder="Add Your Daily Mileage"
-        v-on:input="daysToCompleteSection()"
-        required
-      />
-    </label>
-  </div>
-  <div>
-    <label>Mileage Until Next Resupply
-      <input
-        type="number"
-        v-model.number="totalMileageUntilResupply"
-        placeholder="Add Your Total Mileage For This Section"
-        v-on:input="daysToCompleteSection()"
-        required
-      />
-    </label>
-  </div>
+  Days Until Next Resupply {{  store.daysUntilNextResupply }}<br>
+  Caloric Need for Section {{ store.caloriesUntilNextResupply }}<br>
+  <total-days-and-calories />
 </template>
