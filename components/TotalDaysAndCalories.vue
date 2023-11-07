9bd26abe0b5694506986ef74616f3f9aa9a97098
Dave Bailey <dave.andrew.bailey@gmail.com>
2023-11-07 12:34:08 -0500

    refactor: add TotalDaysAndCalories and use store

---
+++ components/TotalDaysAndCalories.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { useResupplyCalculatorStore } from '../store/useResupplyCalculatorStore';
+
+const store = useResupplyCalculatorStore()
+
+</script>
+
+<template>
+  <div>
+    <label>Calories Per Day
+        <input 
+          type="number"
+          v-model.number="store.dailyCalories"
+          placeholder="Add Your Daily Calories"
+          v-on:input="store.daysToCompleteSection()"
+          required
+        />
+    </label>
+  </div>
+  <div>
+    <label>Miles Per Day
+      <input
+        type="number"
+        v-model.number="store.dailyMileage"
+        placeholder="Add Your Daily Mileage"
+        v-on:input="store.daysToCompleteSection()"
+        required
+      />
+    </label>
+  </div>
+  <div>
+    <label>Mileage Until Next Resupply
+      <input
+        type="number"
+        v-model.number="store.totalMileageUntilResupply"
+        placeholder="Add Your Total Mileage For This Section"
+        v-on:input="store.daysToCompleteSection()"
+        required
+      />
+    </label>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+input {
+  width: 20em;
+}
+</style>
