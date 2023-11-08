04a9987e82dd1a8ae91fdda3e0767fccc000faef
Dave Bailey <dave.andrew.bailey@gmail.com>
2023-11-08 16:09:11 -0500

    style: add global font remove body margin

--- app.vue
+++ app.vue
@@ -4,8 +4,14 @@
   </div>
 </template>
 <style lang="scss">
+
+body {
+  margin: 0;
+}
+
 .app {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue';
+  color: $primary-font;
 }
 
 </style>
