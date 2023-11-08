24751405bfc1706874d1ee7284d16a59d4240387
Dave Bailey <dave.andrew.bailey@gmail.com>
2023-11-08 13:15:45 -0500

    style: add font globally

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
 <template>
-  <NuxtPage />
+  <div class="app">
+    <NuxtPage />
+  </div>
 </template>
+<style lang="scss">
+.app {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',;
+}
+
+</style>
