c0fcf3445b8c1d46eab434f9628a0ab0cfb93b2f
Dave Bailey <dave.andrew.bailey@gmail.com>
2023-11-13 15:10:28 -0500

    feat: add pdf download for daily food list

--- components/resupply/ResupplyDailyFoodList.vue
+++ components/resupply/ResupplyDailyFoodList.vue
@@ -1,27 +1,39 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { exportToPDF } from '#imports'
+
 import { useResupplyCalculatorStore } from '../../store/useResupplyCalculatorStore';
 const store = useResupplyCalculatorStore()
+
+const pdfSection = ref<HTMLElement | null>(null)
 </script>
 
 <template>
-  <div class="daily-food-list" v-if="store.resupplyItemList.length > 0">
-    <div class="daily-food-list__title">DAILY FOOD LIST</div>
-    <ul>
+  <div ref="pdfSection" class="daily-food-list" v-if="store.resupplyItemList.length > 0">
+    <div class="daily-food-list__title">Daily Food List</div>
+    <ul >
       <li v-for="(item, index) in store.resupplyItemList" v-bind:key="index">
-        {{ item.itemName }}: {{  item.servingSize / store.daysUntilNextResupply }} Servings
+        {{ item.itemName }}: {{  (item.servingSize / store.daysUntilNextResupply).toFixed(1) }} Servings
       </li>
     </ul>
+    <button @click="exportToPDF('my-resupply-list.pdf', pdfSection)">
+      Download as PDF
+    </button>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .daily-food-list {
+  background: white;
   display: flex;
   flex-direction: column;
+  margin: 2em 0;
   padding: 2em;
+  box-sizing: border-box;
+  width: 100%;
   
   &__title {
-    font-weight: 600;
+    font-weight: 300;
     font-size: 1.5em;
   }
 }
