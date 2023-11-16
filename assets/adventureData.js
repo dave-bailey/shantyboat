3290ad61d35cea196b6e67738397e6d4979c6fd7
Dave Bailey <dave.andrew.bailey@gmail.com>
2023-11-16 09:04:41 -0500

    feat: add initial mock adventures data

---
+++ assets/adventureData.js
@@ -0,0 +1,74 @@
+export const adventureData = [
+  {
+    title: "Appalachian Trail",
+    description: "April - September 2016",
+    titleImg: "appalachian_trail.jpeg",
+    copy: "The Camino de Santiago, with its historic and sacred trails, has long been a pilgrimage for those seeking adventure, spiritual reflection, and the allure of Spanish culture. While the traditional route takes travelers from St. Jean Pied de Port to Santiago de Compostela, we decided to embark on a unique and inspiring journey, walking in reverse from Muxia to Finisterre, and then to Santiago. This route allowed us to experience the incredible coastal vistas, immerse ourselves in Spanish culture, and complete the necessary 60 kilometers to earn our Compostela.",
+  },
+  {
+    title: " Continental Divide Trail",
+    description: "April - September 2021",
+    titleImg: "continental_divide_trail.jpeg",
+    copy: "The Camino de Santiago, with its historic and sacred trails, has long been a pilgrimage for those seeking adventure, spiritual reflection, and the allure of Spanish culture. While the traditional route takes travelers from St. Jean Pied de Port to Santiago de Compostela, we decided to embark on a unique and inspiring journey, walking in reverse from Muxia to Finisterre, and then to Santiago. This route allowed us to experience the incredible coastal vistas, immerse ourselves in Spanish culture, and complete the necessary 60 kilometers to earn our Compostela.",
+  },
+  {
+    title: "Pacific Crest Trail",
+    description: "May - October 2019",
+    titleImg: "pacific_crest_trail.jpeg",
+    copy: "The Camino de Santiago, with its historic and sacred trails, has long been a pilgrimage for those seeking adventure, spiritual reflection, and the allure of Spanish culture. While the traditional route takes travelers from St. Jean Pied de Port to Santiago de Compostela, we decided to embark on a unique and inspiring journey, walking in reverse from Muxia to Finisterre, and then to Santiago. This route allowed us to experience the incredible coastal vistas, immerse ourselves in Spanish culture, and complete the necessary 60 kilometers to earn our Compostela.",
+  },
+  {
+    title: "Long Trail",
+    description: "September - October 2020",
+    titleImg: "long_trail.jpeg",
+    copy: "The Camino de Santiago, with its historic and sacred trails, has long been a pilgrimage for those seeking adventure, spiritual reflection, and the allure of Spanish culture. While the traditional route takes travelers from St. Jean Pied de Port to Santiago de Compostela, we decided to embark on a unique and inspiring journey, walking in reverse from Muxia to Finisterre, and then to Santiago. This route allowed us to experience the incredible coastal vistas, immerse ourselves in Spanish culture, and complete the necessary 60 kilometers to earn our Compostela.",
+  },
+  {
+    title: "Colorado Trail",
+    description: "August - September 2017",
+    titleImg: "colorado_trail.jpeg",
+    copy: "The Camino de Santiago, with its historic and sacred trails, has long been a pilgrimage for those seeking adventure, spiritual reflection, and the allure of Spanish culture. While the traditional route takes travelers from St. Jean Pied de Port to Santiago de Compostela, we decided to embark on a unique and inspiring journey, walking in reverse from Muxia to Finisterre, and then to Santiago. This route allowed us to experience the incredible coastal vistas, immerse ourselves in Spanish culture, and complete the necessary 60 kilometers to earn our Compostela.",
+  },
+  {
+    title: "John Muir Trail",
+    description: "August - September 2012",
+    titleImg: "john_muir_trail.jpeg",
+    copy: "The Camino de Santiago, with its historic and sacred trails, has long been a pilgrimage for those seeking adventure, spiritual reflection, and the allure of Spanish culture. While the traditional route takes travelers from St. Jean Pied de Port to Santiago de Compostela, we decided to embark on a unique and inspiring journey, walking in reverse from Muxia to Finisterre, and then to Santiago. This route allowed us to experience the incredible coastal vistas, immerse ourselves in Spanish culture, and complete the necessary 60 kilometers to earn our Compostela.",
+  },
+  {
+    title: "Arizona Trail",
+    description: "March - April 2021",
+    titleImg: "arizona_trail.jpeg",
+    copy: "The Camino de Santiago, with its historic and sacred trails, has long been a pilgrimage for those seeking adventure, spiritual reflection, and the allure of Spanish culture. While the traditional route takes travelers from St. Jean Pied de Port to Santiago de Compostela, we decided to embark on a unique and inspiring journey, walking in reverse from Muxia to Finisterre, and then to Santiago. This route allowed us to experience the incredible coastal vistas, immerse ourselves in Spanish culture, and complete the necessary 60 kilometers to earn our Compostela.",
+  },
+  {
+    title: "Camino De Santiago",
+    description: "April 2023",
+    titleImg: "camino.jpeg",
+    copy: "The Camino de Santiago, with its historic and sacred trails, has long been a pilgrimage for those seeking adventure, spiritual reflection, and the allure of Spanish culture. While the traditional route takes travelers from St. Jean Pied de Port to Santiago de Compostela, we decided to embark on a unique and inspiring journey, walking in reverse from Muxia to Finisterre, and then to Santiago. This route allowed us to experience the incredible coastal vistas, immerse ourselves in Spanish culture, and complete the necessary 60 kilometers to earn our Compostela.",
+  },
+  {
+    title: "Laugavegur Trail",
+    description: "August 2022",
+    titleImg: "laugavegur_trail.jpeg",
+    copy: "The Camino de Santiago, with its historic and sacred trails, has long been a pilgrimage for those seeking adventure, spiritual reflection, and the allure of Spanish culture. While the traditional route takes travelers from St. Jean Pied de Port to Santiago de Compostela, we decided to embark on a unique and inspiring journey, walking in reverse from Muxia to Finisterre, and then to Santiago. This route allowed us to experience the incredible coastal vistas, immerse ourselves in Spanish culture, and complete the necessary 60 kilometers to earn our Compostela.",
+  },
+  {
+    title: "New Hampshire 4,000 Footers",
+    description: "June 2022 - November 2023",
+    titleImg: "four_thousand_footers.jpeg",
+    copy: "The Camino de Santiago, with its historic and sacred trails, has long been a pilgrimage for those seeking adventure, spiritual reflection, and the allure of Spanish culture. While the traditional route takes travelers from St. Jean Pied de Port to Santiago de Compostela, we decided to embark on a unique and inspiring journey, walking in reverse from Muxia to Finisterre, and then to Santiago. This route allowed us to experience the incredible coastal vistas, immerse ourselves in Spanish culture, and complete the necessary 60 kilometers to earn our Compostela.",
+  },
+  {
+    title: "South Beyond 6,000 Footers",
+    description: "April 2014 - May 2015",
+    titleImg: "six_thousand_footers.jpeg",
+    copy: "The Camino de Santiago, with its historic and sacred trails, has long been a pilgrimage for those seeking adventure, spiritual reflection, and the allure of Spanish culture. While the traditional route takes travelers from St. Jean Pied de Port to Santiago de Compostela, we decided to embark on a unique and inspiring journey, walking in reverse from Muxia to Finisterre, and then to Santiago. This route allowed us to experience the incredible coastal vistas, immerse ourselves in Spanish culture, and complete the necessary 60 kilometers to earn our Compostela.",
+  },
+  {
+    title: "Colorado 14ers",
+    description: "June 2007 - Current",
+    titleImg: "fourteeners.jpeg",
+    copy: "The Camino de Santiago, with its historic and sacred trails, has long been a pilgrimage for those seeking adventure, spiritual reflection, and the allure of Spanish culture. While the traditional route takes travelers from St. Jean Pied de Port to Santiago de Compostela, we decided to embark on a unique and inspiring journey, walking in reverse from Muxia to Finisterre, and then to Santiago. This route allowed us to experience the incredible coastal vistas, immerse ourselves in Spanish culture, and complete the necessary 60 kilometers to earn our Compostela.",
+  },
+]
